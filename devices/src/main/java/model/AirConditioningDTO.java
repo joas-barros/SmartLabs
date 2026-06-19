e77134daf5569050cedc72a300fa6b7c095230fe
joas-barros <joas.barros14@hotmail.com>
2026-06-19 10:04:56 -0300

    feat: model classes

--- devices/src/main/java/model/AirConditioningDTO.java
+++ devices/src/main/java/model/AirConditioningDTO.java
@@ -1,19 +1,83 @@
 package model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-
 import java.time.Instant;
 
-public record AirConditioningDTO(
-        String lab,
-        String id,
-        Double environmentTemperature,
-        Boolean isOn,
-        Double powerConsumptionInWatts,
-        OperationModeAC operationModeAC,
+public class AirConditioningDTO {
+
+        private String lab;
+        private String id;
+        private Double environmentTemperature;
+        private Boolean isOn;
+        private Double powerConsumptionInWatts;
+        private OperationModeAC operationModeAC;
 
         @JsonProperty("timestamp")
-        Instant timestamp
-) {
+        private Instant timestamp;
 
+        public AirConditioningDTO() {
+        }
+
+        public AirConditioningDTO(String lab, String id) {
+                this.lab = lab;
+                this.id = id;
+                this.timestamp = Instant.now();
+        }
+
+        // Getters e Setters
+        public String getLab() {
+                return lab;
+        }
+
+        public void setLab(String lab) {
+                this.lab = lab;
+        }
+
+        public String getId() {
+                return id;
+        }
+
+        public void setId(String id) {
+                this.id = id;
+        }
+
+        public Double getEnvironmentTemperature() {
+                return environmentTemperature;
+        }
+
+        public void setEnvironmentTemperature(Double environmentTemperature) {
+                this.environmentTemperature = environmentTemperature;
+        }
+
+        public Boolean getIsOn() {
+                return isOn;
+        }
+
+        public void setIsOn(Boolean isOn) {
+                this.isOn = isOn;
+        }
+
+        public Double getPowerConsumptionInWatts() {
+                return powerConsumptionInWatts;
+        }
+
+        public void setPowerConsumptionInWatts(Double powerConsumptionInWatts) {
+                this.powerConsumptionInWatts = powerConsumptionInWatts;
+        }
+
+        public OperationModeAC getOperationModeAC() {
+                return operationModeAC;
+        }
+
+        public void setOperationModeAC(OperationModeAC operationModeAC) {
+                this.operationModeAC = operationModeAC;
+        }
+
+        public Instant getTimestamp() {
+                return timestamp;
+        }
+
+        public void setTimestamp(Instant timestamp) {
+                this.timestamp = timestamp;
+        }
 }
--- devices/src/main/java/model/ProjectorDTO.java
+++ devices/src/main/java/model/ProjectorDTO.java
@@ -1,6 +1,27 @@
 package model;
 
-public record ProjectorDTO(
+import com.fasterxml.jackson.annotation.JsonProperty;
 
-) {
+import java.time.Instant;
+
+public class ProjectorDTO {
+    private String lab;
+    private String id;
+    private Boolean isOn;
+    private Long usageTimeInMinutes;
+    private Double internalTemperature;
+    private String activeVideoInput;
+    private Double powerConsumptionInWatts;
+
+    @JsonProperty("timestamp")
+    private Instant timestamp;
+
+    public ProjectorDTO() {
+    }
+
+    public ProjectorDTO(String id, String lab) {
+        this.id = id;
+        this.lab = lab;
+        this.timestamp = Instant.now();
+    }
 }
