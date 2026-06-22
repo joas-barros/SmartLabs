1168858e84a00ff3c7491ffdb027d8245f7a9107
joas-barros <joas.barros14@hotmail.com>
2026-06-22 16:56:03 -0300

    feat: init ms-digital-twins module

---
+++ backend/ms-digital-twins/src/main/java/br/ufersa/iot/backend/ms_digital_twins/MsDigitalTwinsApplication.java
@@ -0,0 +1,13 @@
+package br.ufersa.iot.backend.ms_digital_twins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsDigitalTwinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsDigitalTwinsApplication.class, args);
+	}
+
+}
---
+++ backend/ms-digital-twins/src/test/java/br/ufersa/iot/backend/ms_digital_twins/MsDigitalTwinsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufersa.iot.backend.ms_digital_twins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsDigitalTwinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
