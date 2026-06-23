b22c118152870c093e8492aa39b4541fe7ccf2bd
joas-barros <joas.barros14@hotmail.com>
2026-06-22 22:05:31 -0300

    feat: client http

---
+++ cliente-http (1)/cliente-http/src/main/java/br/ufersa/iot/cliente_http/ClienteHttpApplication.java
@@ -0,0 +1,13 @@
+package br.ufersa.iot.cliente_http;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteHttpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteHttpApplication.class, args);
+	}
+
+}
---
+++ cliente-http (1)/cliente-http/src/test/java/br/ufersa/iot/cliente_http/ClienteHttpApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufersa.iot.cliente_http;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteHttpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
